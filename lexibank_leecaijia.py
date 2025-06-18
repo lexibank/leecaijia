bec4dc549b9e0a2a5d675a75e0c394d261e1fa0d
lingulist <mattis.list@lingulist.de>
2025-06-18 06:46:48 +0200

    update new files

--- lexibank_leecaijia.py
+++ lexibank_leecaijia.py
@@ -9,21 +9,14 @@ from pylexibank import FormSpec
 class Dataset(BaseDataset):
     dir = pathlib.Path(__file__).parent
     id = "leecaijia"
+    writer_options = dict(keep_languages=False, keep_parameters=False)
 
     def cmd_makecldf(self, args):
-        # add bib
+        # (1) add bib
         args.writer.add_sources()
         args.log.info("added sources")
 
-        corrections = {
-                "tsh": "tsʰ",
-                "tɕh": "tɕʰ",
-                "kh": "kʰ",
-                "th": "tʰ",
-                "ph": "pʰ",
-                }
-
-        # add concept
+        # (2) add concepts
         concepts = {}
         for concept in self.conceptlists[0].concepts.values():
             idx = concept.id.split("-")[-1] + "_" + slug(concept.english)
@@ -36,18 +29,29 @@ class Dataset(BaseDataset):
             concepts[concept.id] = idx
         args.log.info("added concepts")
 
-        # add language
+        # (3) add language
         args.writer.add_language(
                 ID="Caijia",
                 Glottocode="caij1234",
                 Name="Caijia"
                 )
         args.log.info("added languages")
+        
+        # (4) add forms
+        # provide corrections for the data
+        corrections = {
+                "tsh": "tsʰ",
+                "tɕh": "tɕʰ",
+                "kh": "kʰ",
+                "th": "tʰ",
+                "ph": "pʰ",
+                }
 
         # read in data
         data = self.raw_dir.read_csv(
                 "data.tsv", 
                 delimiter="\t", dicts=True)
+
         # extend corrections
         for i, row in enumerate(data):
             form = row["Caijia (segments  separated)"]
